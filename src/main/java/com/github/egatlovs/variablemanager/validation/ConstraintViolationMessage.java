7b9eb70a35efb153a37588caaebeb7c3e3ee4913
Alexander Bogner <alex.bogner36@googlemail.com>
2018-04-22 20:38:18 +0200

    feat: add documentation for validation classes

--- src/main/java/com/github/egatlovs/variablemanager/validation/ConstraintViolationMessage.java
+++ src/main/java/com/github/egatlovs/variablemanager/validation/ConstraintViolationMessage.java
@@ -4,14 +4,35 @@ import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 
+/**
+ * <b>ConstraintViolationMessage</b></br>
+ * </br>
+ * ConstraintViolationMessage uses the BeanValidations ConstraintViolations to
+ * create a String representing each validation failure of an ExecutionEntity.
+ * 
+ * @author egatlovs
+ */
 public class ConstraintViolationMessage {
 
 	private final Set<ConstraintViolation<Object>> constraintViolations;
 
+	/**
+	 * Constructor creating a Constraint Violation Message out of a Set of
+	 * ConstraintViolations.
+	 * 
+	 * @param constraintViolations
+	 *            - The violations occured while validating
+	 */
 	public ConstraintViolationMessage(Set<ConstraintViolation<Object>> constraintViolations) {
 		this.constraintViolations = constraintViolations;
 	}
 
+	/**
+	 * Returns a message containing each ConstraintViolation in a more readable
+	 * representation.
+	 * 
+	 * @return - The formatted ConstraintViolations
+	 */
 	public String getMessage() {
 		StringBuilder sb = new StringBuilder("Constraint Violations occured: \n");
 		for (ConstraintViolation<Object> constraintViolation : constraintViolations) {
