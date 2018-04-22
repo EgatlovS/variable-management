ca90987832439e26d087c6cacb3231fc7c005a71
Alexander Bogner <alex.bogner36@googlemail.com>
2018-04-22 20:30:28 +0200

    feat: add documentation to producer

--- src/main/java/com/github/egatlovs/variablemanager/producers/ExecutionProducer.java
+++ src/main/java/com/github/egatlovs/variablemanager/producers/ExecutionProducer.java
@@ -5,8 +5,21 @@ import javax.enterprise.inject.Produces;
 import org.camunda.bpm.engine.context.DelegateExecutionContext;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 
+/**
+ * <b>ExecutionProducer</b></br>
+ * </br>
+ * This class provides the current DelegateExecution if present otherwise null
+ * is returned.
+ * 
+ * @author egatlovs
+ */
 public class ExecutionProducer {
 
+	/**
+	 * Produces a DelegateExecution.
+	 * 
+	 * @return - The current DelegateExecution or null if no execution is present
+	 */
 	@Produces
 	public DelegateExecution getDelegateExecution() {
 		return DelegateExecutionContext.getCurrentDelegationExecution();
