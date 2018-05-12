50749aab72adf706089d09e29adca5dc7b660c81
Alexander Bogner <alex.bogner36@googlemail.com>
2018-05-12 19:26:26 +0200

    feat: add behaviour tests for executionproducer

---
+++ src/test/java/com/github/egatlovs/unit/producers/ExecutionProducerTest.java
@@ -0,0 +1,15 @@
+package com.github.egatlovs.unit.producers;
+
+import com.github.egatlovs.variablemanager.producers.ExecutionProducer;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class ExecutionProducerTest {
+
+    @Test
+    public void Should_Return_Null_If_No_Execution_Is_Present(){
+        ExecutionProducer producer = new ExecutionProducer();
+        Assertions.assertThat(producer.getDelegateExecution()).isNull();
+    }
+
+}
